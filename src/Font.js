b0f712e2b6634129e855c4248876e5c6ea4607f2
Shiva Nandan <i.am@seekshiva.in>
2018-10-30 11:07:36 +0530

    Ability to mark a font as downloaded

--- src/Font.js
+++ src/Font.js
@@ -70,6 +70,10 @@ export function isLoaded(name: string): boolean {
 export function isLoading(name: string): boolean {
   return loadPromises.hasOwnProperty(name);
 }
+                     
+export function addLoadedFont(name: string): void {
+  loaded[name] = true;
+}
 
 export async function loadAsync(
   nameOrMap: string | { [string]: FontSource } | Array<{ [string]: FontSource }>,
